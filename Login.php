27b40590b0a19257f5e8a026d3989764bcdd3d06
Seth Connell <sethjordanconnell@gmail.com>
2017-07-21 22:15:54 -0500

    Added Html Elements to Login.php
    Added basic html elements to login php page.

---
+++ Login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+    <form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+    </form>
+</body>
+
+</html>
