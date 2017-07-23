8e00da7d749a9b88c70c5014d10224029bfc9581
Seth Connell <sethjordanconnell@gmail.com>
2017-07-22 22:14:55 -0500

    Added Encryption
    Added a bit of encryption for the passwords in database.

--- Login.php
+++ Login.php
@@ -1,6 +1,18 @@
 <?php
+    // Force HTTPS for security
+    if($_SERVER["HTTPS"] != "on") {
+        $pageURL = "Location: https://";
+        if ($_SERVER["SERVER_PORT"] != "80") {
+            $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+        } else {
+            $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        }
+        header($pageURL);
+    }
     if (isset($_POST["name"])) {
-		$name = $_POST["name"];
+		$user_password = $_POST["name"];
+		# Hashing the name (I'm going to change it to a password later).
+		$user_password = password_hash($user_password, PASSWORD_DEFAULT, ['cost' => 12]);
         echo "<h1>Screw you " . $name . "</h1>"; 
 		$servername = "127.0.0.1";
 		$username = "seth";
@@ -27,7 +39,7 @@
 		)";
 		$send_query($conn, $sql);
 		$sql = "INSERT INTO persons (name)
-		VALUES ('$name');";
+		VALUES ('$user_password');";
 		$send_query($conn, $sql);
 		mysqli_close($conn);
     }
